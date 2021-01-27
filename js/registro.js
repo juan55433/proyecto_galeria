cb4816c2dab644aaa5f56f0b01d7f04c302897b2
Juan <juan.ochoad@upb.edu.co>
2021-01-27 10:59:50 -0500

    crear usuario con parametros

--- js/registro.js
+++ js/registro.js
@@ -2,30 +2,33 @@
 const formregisterUI = document.querySelector('#form-ing');
 let arrayRegistro = [];
 //funciones
-const CrearUsuario = (usuario) => {
+const CrearUsuario = (user, pw, pwrp, email, number) => {
     let item = {
-        usuario: '',
-        contraseña:'',
-        contraseñarep:'',
-        email:'',
-        numeroTelefono:''
+        user: user,
+        pw:pw,
+        pwrp:pwrp,
+        email:email,
+        number:number
     }   
     arrayRegistro.push(item);
     return item;
 }
 
+const GuardarDB = () =>{
+    
+}
 //EventListener
 
 formregisterUI.addEventListener('submit',(e) =>{
     e.preventDefault();
+    
     let userUI = document.querySelector('#user').value;
     let pwUI = document.querySelector('#password').value;
     let pwrpUI = document.querySelector('#password-repeat').value;
     let emailUI = document.querySelector('#email').value;
     let numberUI = document.querySelector('#number').value;
-    console.log(userUI);
-    console.log(pwUI);
-    console.log(pwrpUI);
-    console.log(emailUI);
-    console.log(numberUI);
-})
+    
+    CrearUsuario(userUI, pwUI, pwrpUI, emailUI,numberUI);
+    formregisterUI.reset(); 
+    
+});
