a2ff9e7cc0a8deb3953813ca1eac387480f3bc26
Juan <juan.ochoad@upb.edu.co>
2021-02-01 19:25:11 -0500

    validacion registro

--- js/registro.js
+++ js/registro.js
@@ -18,6 +18,35 @@ const GuardarDB = () =>{
 }
 
 //EventListener     
+function validar_pw(pwUI){
+    //validacion pw
+    var mays = false;
+    var minus = false;
+    var numr = false;
+    var symb = false;
+    if(pwUI.length >= 8){
+        for(var i = 0; i <pwUI.length;i++){
+            if(pwUI.charCodeAt(i) >= 65 && pwUI.charCodeAt(i) <= 90){
+                mays = true;
+            }
+            else if(pwUI.charCodeAt(i) >= 97 && pwUI.charCodeAt(i) <= 122){
+                minus = true;
+            }
+            else if(pwUI.charCodeAt(i) >= 48 && pwUI.charCodeAt(i) <= 57){
+                numr = true;
+            }
+            else{
+                symb = true;
+            }
+        }
+        if(mays == true && minus == true && numr == true && symb == true){
+            return true;
+        }
+    }
+    return false;
+    //validacion pw
+}
+
 
 formregisterUI.addEventListener('submit',(e) =>{
     e.preventDefault();
@@ -27,6 +56,8 @@ formregisterUI.addEventListener('submit',(e) =>{
     let pwrpUI = document.querySelector('#password-repeat').value;
     let emailUI = document.querySelector('#email').value;
     let numberUI = document.querySelector('#number').value;
+    
+    
     if(userUI.length == 0 || pwUI.length == 0 || pwrpUI.length == 0 || emailUI.length == 0 || isNaN(numberUI)) 
     {
         window.alert("Todos los caracteres son obligatorios!");
@@ -35,11 +66,13 @@ formregisterUI.addEventListener('submit',(e) =>{
     else if(pwUI != pwrpUI){
         window.alert("Las contraseñas deben coincidir");
     }
+    else if(validar_pw(pwUI) == false){
+        window.alert("Contraseña debe tener por lo menos 8 caracteres, 1 mayuscula, 1 minuscula, 1 simbolo, 1 número")   
+    }
     else{
         CrearUsuario(userUI, pwUI, pwrpUI, emailUI,numberUI);
         GuardarDB();
         formregisterUI.reset(); 
-        window.open("login.html", "_self");  
-        
+        window.open("login.html", "_self");     
     }
 });
