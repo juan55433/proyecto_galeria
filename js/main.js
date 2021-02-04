bd112e81b00d2ce8eff97cb87b6b07cd39b768cf
Juan <juan.ochoad@upb.edu.co>
2021-02-04 07:19:08 -0500

    se agregó window.onload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 /*eslint-env es6*/
-const button = document.querySelector('button');
-const popup = document.querySelector('.popup-wrapper');
-const close = document.querySelector('.popup-close');
-button.addEventListener('click', () => {
+function inicio(){
+    const button = document.querySelector('button');
+    const popup = document.querySelector('.popup-wrapper');
+    const close = document.querySelector('.popup-close');
+    button.addEventListener('click', () => {
     popup.style.display = 'block';
-});
+    });
 
-close.addEventListener('click', () => {
-    popup.style.display = 'none';
-});
-popup.addEventListener('click', e => {
-    // console.log(e);
-    if(e.target.className === 'popup-wrapper') {
+    close.addEventListener('click', () => {
         popup.style.display = 'none';
-    }
-});
+    });
+    popup.addEventListener('click', e => {
+        // console.log(e);
+        if(e.target.className === 'popup-wrapper') {
+            popup.style.display = 'none';
+        }
+    })
+}
+window.onload = inicio;
+inicio();
