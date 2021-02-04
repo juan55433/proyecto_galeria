f720c2e6f60bdaab9a21a4812d79d53a0cfe31ef
Juan <juan.ochoad@upb.edu.co>
2021-02-04 07:50:58 -0500

    window on load

--- js/registro.js
+++ js/registro.js
@@ -1,23 +1,24 @@
-//variables globales
-const formregisterUI = document.querySelector('#form-ing');
-let arrayRegistro = [];
+//Funcion onload
+window.onload = inicio;
 
-const CrearUsuario = (user, pw, pwrp, email, number) => {
-    let item = {
-        user: user,
-        pw:pw,
-        pwrp:pwrp,
-        email:email,
-        number:number
-    }   
-    arrayRegistro.push(item);
-    return item;
-}
-const GuardarDB = () =>{
-    localStorage.setItem('registro',JSON.stringify(arrayRegistro));
-}
+function inicio(){
+    const formregisterUI = document.querySelector('#form-ing');
+    let arrayRegistro = [];
 
-//EventListener     
+    const CrearUsuario = (user, pw, pwrp, email, number) => {
+        let item = {
+            user: user,
+            pw:pw,
+            pwrp:pwrp,
+            email:email,
+            number:number
+        }   
+        arrayRegistro.push(item);
+        return item;
+    }
+    const GuardarDB = () =>{
+        localStorage.setItem('registro',JSON.stringify(arrayRegistro));
+    }
 function validar_pw(pwUI){
     //validacion pw
     var mays = false;
@@ -48,31 +49,34 @@ function validar_pw(pwUI){
 }
 
 
-formregisterUI.addEventListener('submit',(e) =>{
-    e.preventDefault();
-    
-    let userUI = document.querySelector('#user').value;
-    let pwUI = document.querySelector('#password').value;
-    let pwrpUI = document.querySelector('#password-repeat').value;
-    let emailUI = document.querySelector('#email').value;
-    let numberUI = document.querySelector('#number').value;
-    
-    
-    if(userUI.length == 0 || pwUI.length == 0 || pwrpUI.length == 0 || emailUI.length == 0 || isNaN(numberUI)) 
-    {
-        window.alert("Todos los caracteres son obligatorios!");
-        formregisterUI.reset(); 
-    }
-    else if(pwUI != pwrpUI){
-        window.alert("Las contraseñas deben coincidir");
-    }
-    else if(validar_pw(pwUI) == false){
-        window.alert("Contraseña debe tener por lo menos 8 caracteres, 1 mayuscula, 1 minuscula, 1 simbolo, 1 número")   
-    }
-    else{
-        CrearUsuario(userUI, pwUI, pwrpUI, emailUI,numberUI);
-        GuardarDB();
-        formregisterUI.reset(); 
-        window.open("login.html", "_self");     
-    }
-});
+    formregisterUI.addEventListener('submit',(e) =>{
+        e.preventDefault();
+
+        let userUI = document.querySelector('#user').value;
+        let pwUI = document.querySelector('#password').value;
+        let pwrpUI = document.querySelector('#password-repeat').value;
+        let emailUI = document.querySelector('#email').value;
+        let numberUI = document.querySelector('#number').value;
+
+
+        if(userUI.length == 0 || pwUI.length == 0 || pwrpUI.length == 0 || emailUI.length == 0 || isNaN(numberUI)) 
+        {
+            window.alert("Todos los caracteres son obligatorios!");
+            formregisterUI.reset(); 
+        }
+        else if(pwUI != pwrpUI){
+            window.alert("Las contraseñas deben coincidir");
+        }
+        else if(validar_pw(pwUI) == false){
+            window.alert("Contraseña debe tener por lo menos 8 caracteres, 1 mayuscula, 1 minuscula, 1 simbolo, 1 número")   
+        }
+        else{
+            CrearUsuario(userUI, pwUI, pwrpUI, emailUI,numberUI);
+            GuardarDB();
+            formregisterUI.reset(); 
+            window.open("login.html", "_self");     
+        }
+    });
+}
+
+
